924bb4eebb28284f5203b9bc56303244ab5b8db4
DamikaAlwis-Gif <damikaa@cse.mrt.ac.lk>
2024-03-29 12:05:21 +0530

    // handeling local images

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
+const express = require("express");
+const bodyParser = require("body-parser");
 
 // Load the .env file if it exists
 require("dotenv").config();
 
-
-const express = require('express'); 
-
 // import the router
- const imageRouter = require("./routes/compVisionRouter");
-
- 
+const imageRouter = require("./routes/compVisionRouter");
 
 const app = express();
-app.use(express.json());
 
-const port = process.env.PORT || 3000;
-app.listen(port , () => {console.log(`Image analyze service running on port: ${port}`);})
+
+app.use(bodyParser.json());
+// Parse URL-encoded bodies (as sent by HTML forms)
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const port = process.env.PORT || 3000;// app runing port number
+
+app.listen(port, () => {
+  console.log(`Image analyze service running on port: ${port}`);
+});
 
 app.use("/api", imageRouter);
 
